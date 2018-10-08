eae4f47cf481eeb1de0d95c35fff68a738c28aa3
juan pablo rocha santelices <juan.rocha211@hotmail.com>
2018-10-08 02:13:39 -0300

    prueba_roche

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Rocha le cule edionde
+                    Chaparro tiene las meas tetas!!!
                 </div>
 
                 <div class="links">
